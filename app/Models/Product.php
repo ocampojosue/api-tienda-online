1b64b07c27f84c46ae1cc533bf38b4f9f49245ff
ocampojosue <ocampojosue08@gmail.com>
2021-11-28 21:02:05 -0400

    Add tag_id to model Tag

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'cost', 'price', 'stock', 'category_id'];
+    protected $fillable = ['name', 'cost', 'price', 'stock', 'category_id','tag_id'];
 }
